44cae57bc64956658d75da434d2bd0924c06e0a2
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2021-10-09 21:18:37 +0200

    bf

--- 08/parcel-project-template-main/src/js/02-video.js
+++ 08/parcel-project-template-main/src/js/02-video.js
@@ -1,1 +1,17 @@
+import '../../node_modules/@vimeo/player/dist/player.js';
+import {throttle} from 'lodash/throttle';
+
+ const iframe=document.querySelector('iframe');
+const player=new Vimeo.Player(iframe);
+const onPlay=e=>{
+  try{
+  localStorage.setItem("videoplayer-current-time",`${e.seconds}`);
+  console.log(localStorage.getItem("videoplayer-current-time"))
+  }catch(error){
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
+
+player.on('timeupdate', onPlay);
 
