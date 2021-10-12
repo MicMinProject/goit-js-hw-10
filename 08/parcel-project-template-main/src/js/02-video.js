448f40ae200dedf5d40c1975514db0b6badd8963
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2021-10-12 02:28:10 +0200

    de

--- 08/parcel-project-template-main/src/js/02-video.js
+++ 08/parcel-project-template-main/src/js/02-video.js
@@ -1,17 +1,24 @@
 import Player from '@vimeo/player';
-import {throttle} from 'lodash/throttle';
+import throttle from 'lodash/throttle';
 
  const iframe=document.querySelector('iframe');
 const player=new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY="videoplayer-current-time";
 const onPlay=e=>{
   try{
-  localStorage.setItem("videoplayer-current-time",`${e.seconds}`);
-  console.log(localStorage.getItem("videoplayer-current-time"))
+  localStorage.setItem(LOCALSTORAGE_KEY,`${e.seconds}`);
+  console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+  
   }catch(error){
     console.log(error.name);
     console.log(error.message);
   }
 }
 
-player.on('timeupdate', onPlay);
+
+player.on('timeupdate', throttle(onPlay, 2000));
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+
+
+
 
--- 08/parcel-project-template-main/src/js/03-feedback.js
+++ 08/parcel-project-template-main/src/js/03-feedback.js
@@ -1,1 +1,30 @@
 
+
+
+const input=document.querySelector('input');
+const textarea=document.querySelector('textarea');
+const button=document.querySelector('buttton');
+const LOCALSTORAGE_KEY="feedback-form-state";
+const data={
+  email: "",
+  message: ""
+}
+
+const onInput=e=>{
+e.preventDefault();
+
+data.email=e.value;
+localStorage.setItem(LOCALSTORAGE_KEY,JSON.stringify(data));
+console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+}
+
+const onTextarea=e=>{
+e.preventDefault();
+
+data.message=e.value;
+localStorage.setItem(LOCALSTORAGE_KEY,JSON.stringify(data));
+console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+}
+
+input.addEventListener('input', onInput);
+textarea.addEventListener('input', onTextarea);
